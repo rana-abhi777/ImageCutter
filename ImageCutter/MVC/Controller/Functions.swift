f8aec53ec80833508eeb3cd429e5c2537a9b8b31
rana-abhi777 <abhisekrana777@gmail.com>
2017-03-17 12:04:57 +0530

    timer and image cropping done Left - Now looking for the drag and drop of images from one imageView to another.

---
+++ ImageCutter/MVC/Controller/Functions.swift
@@ -0,0 +1,9 @@
+//
+//  Functions.swift
+//  ImageCutter
+//
+//  Created by Sierra 4 on 17/03/17.
+//  Copyright © 2017 code-brew. All rights reserved.
+//
+
+import Foundation
--- ImageCutter/MVC/Controller/ViewController.swift
+++ ImageCutter/MVC/Controller/ViewController.swift
@@ -10,27 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var imageSet: UIImage = UIImage(named: "background1")!
-    var imageCroppedArray = [UIImage]()
+    var imageUploaded = UIImage()
+    var imageSplitArray = [UIImage]()
     
-    var width = 0
-    var height = 0
-    var widthSmallView = 0
-    var heightSmallView = 0
+    var widthImageUploaded: CGFloat?
+    var heightImageUploaded: CGFloat?
     
-    var heightForEachSplit = 0
-    var widthForEachSplit = 0
+    let rowsSuper = 3
+    let columnsSuper = 3
     
-    var rows = 3
-    var columns = 3
+    var startClock = Timer()
+    var timer = 5
     
     @IBOutlet var imgLowerViewCollection: [UIImageView]!
-    
-    
-    
-    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var lblTimeLeftTitle: UILabel!
+    @IBOutlet weak var lblTimeLeftValue: UILabel!
     @IBOutlet weak var viewImageView: UIImageView!
-    
     
     func genericAlertMessage(theTitle: String, theMessage: String)
     {
@@ -48,182 +43,73 @@ class ViewController: UIViewController {
         alertController.addAction(OKAction)
         self.present(alertController, animated: true, completion: nil)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        width = Int(viewImageView.frame.size.width)
-        height = Int(viewImageView.frame.size.height)
-        print("the width is : \(width)")
-        print("the height is : \(height)")
-        //resetTheViewSize(viewName: splitView1, widthToSet: CGFloat(200), heightToSet: CGFloat(200))
-        widthSmallView = Int(viewImageView.frame.size.width)
-        heightSmallView = Int(viewImageView.frame.size.height)
-        print("The width of smaller view is \(widthSmallView)")
-        print("The height of smaller view is \(heightSmallView)")
-        splitImagesDimensions()
-        
-        // patterns of collection array
-//        setdifferentColorbackGroundsToCollection()
-        
-//        // adding a timer delay =====================================================
-//        let when = DispatchTime.now() + 5 // change 2 to desired number of seconds
-//        DispatchQueue.main.asyncAfter(deadline: when) {
-//            self.genericAlertMessage(theTitle: "Alert!!!", theMessage: "Your times up Bruh!!!\nRemember Big Bro is watching!!!\nNo Cheating.")
-//        }
-//        // ==============================================================================
-        cropImagesAll()
-        setdifferentColorbackGroundsToCollection()
-        
+        imageUploaded = viewImageView.image!
+        widthImageUploaded = imageUploaded.size.width
+        heightImageUploaded = imageUploaded.size.height
+        //startTimer()
+        startClock = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.countDown), userInfo: nil, repeats: true)
+        //addDelay()
         
     }
-    // different background color setting to the collection array
-    func setdifferentColorbackGroundsToCollection() {
-//        let colorsArray = [UIColor.orange, UIColor.red, UIColor.black, UIColor.blue, UIColor.brown, UIColor.cyan, UIColor.darkGray, UIColor.green, UIColor.magenta]
-        for index in 0...(imgLowerViewCollection.count - 1) {
-            imgLowerViewCollection[index].contentMode = .scaleAspectFit
-            imgLowerViewCollection[index].image = imageCroppedArray[index]
-//            imgLowerViewCollection[index].backgroundColor = colorsArray[index]
+    func startTimer() {
+        startClock = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.countDown), userInfo: nil, repeats: true)
+    }
+    func countDown() {
+        lblTimeLeftValue.text = "\(timer)s"
+        if timer == 0 {
+           startClock.invalidate()
+           self.genericAlertMessage(theTitle: "Alert!!!", theMessage: "Your times up Bruh!!!\nRemember Big Bro is watching!!!\nNo Cheating.")
+           
+        }
+        timer = timer - 1
+        
+    }
+    func addDelay() {
+        // adding a timer delay =====================================================
+        let when = DispatchTime.now() + 10 // change 2 to desired number of seconds
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            self.genericAlertMessage(theTitle: "Alert!!!", theMessage: "Your times up Bruh!!!\nRemember Big Bro is watching!!!\nNo Cheating.")
+        }
+        // ==============================================================================
+    }
+    func splitImage(image: UIImage, heightOfImage: CGFloat, widthOfImage: CGFloat) {
+        var xPos: CGFloat = 0.0
+        var  yPos: CGFloat = 0.0
+        
+        let widthOfSplitImages = CGFloat(Float(widthOfImage) / Float(columnsSuper))
+        let heightOfSplitImages = CGFloat(Float(heightOfImage) / Float(rowsSuper))
+        
+        for row in 0..<rowsSuper {
+            yPos = yPos + CGFloat(Float(row) * Float(heightOfSplitImages))
+            for col in 0..<columnsSuper {
+                xPos = xPos + CGFloat(Float(col) * Float(widthOfSplitImages))
+                let rect = CGRect(x: xPos, y: yPos, width: widthOfSplitImages, height: heightOfSplitImages)
+                let imageRef: CGImage = (image.cgImage?.cropping(to: rect))!
+                let image: UIImage = UIImage(cgImage: imageRef, scale: 1, orientation: image.imageOrientation)
+                
+                //saving the cropped image in an array
+                imageSplitArray.append(image)
+                xPos = 0.0
+                yPos = 0.0
+            }
         }
     }
+    
     @IBAction func btnBackClick(_ sender: Any) {
         if let navController = self.navigationController {
             navController.popViewController(animated: true)
         }
     }
     
-    func resetTheViewSize(viewName: UIView, widthToSet: CGFloat, heightToSet: CGFloat) {
-        viewName.frame.size.width = widthToSet
-        viewName.frame.size.height = heightToSet
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    func splitImagesDimensions() {
-        heightForEachSplit = height / rows
-        widthForEachSplit = width / columns
-        print("height for each is  : \(heightForEachSplit)")
-        print("width for each is : \(widthForEachSplit)")
-    }
-//    func cropSinglePart() -> UIImage?{
-//        var xPos:CGFloat = 0.0
-//        var yPos:CGFloat = 0.0
-//        let width: CGFloat = CGFloat(self.width)
-//        let height: CGFloat = CGFloat(self.height)
-//        
-//        let rect = CGRect(x: xPos, y: yPos, width: width, height: height)
-//        guard let cgImage = cgImage else {
-//            return nil
-//        }
-//        guard let newCgImage = cgImage.cropping(to: rect) else {
-//            return nil
-//        }
-//        
-//        
-//        return
-//    }
-    func cropImageComplete() {
-        
-    }
-    func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
-//        let contextImage: UIImage = UIImage(cgImage: image.cgImage!)
-        
-//        let contextSize: CGSize = contextImage.size
-        let posX: CGFloat = 0.0
-        let posY: CGFloat = 0.0
-//        var cgwidth: CGFloat = CGFloat(width)
-//        var cgheight: CGFloat = CGFloat(height)
-        // See what size is longer and create the center off of that
-        
-//        if contextSize.width > contextSize.height {
-//            posX = ((contextSize.width - contextSize.height) / 2)
-//            posY = 0
-//            cgwidth = contextSize.height
-//            cgheight = contextSize.height
-//        } else {
-//            posX = 0
-//            posY = ((contextSize.height - contextSize.width) / 2)
-//            cgwidth = contextSize.width
-//            cgheight = contextSize.width
-//        }
-        let rect: CGRect = CGRect(x: posX, y: posY, width: CGFloat(300), height: CGFloat(300))
-        // Create bitmap image from context using the rect
-        let imageRef: CGImage = imageSet.cgImage!.cropping(to: rect)!
-        
-        // Create a new image based on the imageRef and rotate back to the original orientation
-        let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
-        
-        return image
-    }
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    func cropImagesAll() {
-        var xPos: CGFloat = 0.0
-        var yPos: CGFloat = 0.0
-        let widthInner = CGFloat(width / 3)
-        print(widthInner)
-        let heightInner = CGFloat(height / 3)
-        print(heightInner)
-        // row variable is kept for debugging of the for loop
-        for row in 0..<3 {
-            // col variable is kept for debugging of the for loop
-            for col in 0..<3 {
-                
-                let rect: CGRect = CGRect(x: xPos, y: yPos, width: CGFloat(widthInner), height: CGFloat(heightInner))
-                // Create bitmap image from context using the rect
-                let imageRef: CGImage = imageSet.cgImage!.cropping(to: rect)!
-                
-                // Create a new image based on the imageRef and rotate back to the original orientation
-                let image: UIImage = UIImage(cgImage: imageRef, scale: 1, orientation: imageSet.imageOrientation)
-                
-                imageCroppedArray.append(image)
-//                let rect = CGRect(x: xPos, y: yPos, width: width, height: height)
-//                let imageRef: CGImage? = imageSet.cgImage?.cropping(to: rect)
-//                let image: UIImage = UIImage(cgImage: imageRef, scale: imageSet.scale, orientation: imageSet.imageOrientation)
-//                imageCroppedArray.append(image)
-                xPos = xPos + widthInner
-                
-            }
-            yPos = yPos + heightInner
-        }
-        print("Cropping done")
-    }
-
-    
-//    func getImagesFrom(_ image: UIImage) -> [Any] {
-//        var images = [Any]()
-//        //var imageSize: CGSize = image.size
-//        var xPos: CGFloat = 0.0
-//        let yPos: CGFloat = 0.0
-//        let width: CGFloat = CGFloat(self.width)
-//        let height: CGFloat = CGFloat(self.height)
-//        for y in 0..<columns {
-//            xPos = 0.0
-//            for x in 0..<rows {
-//                let rect = CGRect(x: xPos, y: yPos, width: width, height: height)
-//                let cImage: CGImage? = image.cgImage?.cropping(to: rect)
-//                let dImage = UIImage(cgImage: cImage!)
-//                let imageView = UIImageView(frame: CGRect(x: CGFloat(x * Int(width)), y: CGFloat(y * Int(height)), width: width, height: height))
-//                imageView.image = dImage
-//                imageView.layer.borderColor = UIColor.black.cgColor
-//                imageView.layer.borderWidth = 1.0
-//                images.append(image)
-//            }
-//        }
-//       return images
-//    }
-    
-    
-//    func splitImage() {
-//        let imagesSplitArray = getImagesFrom(imageSet)
-//    }
-
     @IBAction func btnSplitImage(_ sender: Any) {
-        
-//        let image = cropToBounds(image: imageSet, width: Double(widthSmallView), height: Double(heightSmallView))
-//        imgView.contentMode = .scaleAspectFit
-//        imgView.image = image
+        splitImage(image: imageUploaded, heightOfImage: heightImageUploaded!, widthOfImage: widthImageUploaded!)
+        for index in 0...(imgLowerViewCollection.count - 1) {
+            imgLowerViewCollection[index].image = imageSplitArray[index]
+        }
     }
 }
 
